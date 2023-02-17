cb2d0d821205db762617f9c65da526a852018b15
frenato00 <up201908044@up.pt>
2023-02-17 12:27:47 +0000

    feat: add sprite movement and animation

---
+++ Campus Chaos/Assets/Scripts/PlayerAnimator.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerAnimator : MonoBehaviour
+{
+
+    Animator animator;
+    PlayerMovement playerMovement;
+    SpriteRenderer spriteRenderer;
+    // Start is called before the first frame update
+    void Start()
+    {
+        animator = GetComponent<Animator>();
+        playerMovement = GetComponent<PlayerMovement>();
+        spriteRenderer = GetComponent<SpriteRenderer>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playerMovement.moveDir.x!=0 || playerMovement.moveDir.y!=0)
+        {
+            animator.SetBool("Move", true);
+            SpriteDirectionChecker();
+        }
+        else{
+            animator.SetBool("Move", false);
+        }
+    }
+
+    void SpriteDirectionChecker()
+    {
+        if(playerMovement.lastHorizontalVector <0){
+            spriteRenderer.flipX = true;
+        }
+        else{
+            spriteRenderer.flipX = false;
+        }
+    }
+}
