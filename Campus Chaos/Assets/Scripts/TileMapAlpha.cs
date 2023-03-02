e765b51f988dbbe8a0a92c25365dbab74aef29a8
frenato00 <up201908044@up.pt>
2023-03-02 13:11:41 +0000

    feat: add collision with objs

---
+++ Campus Chaos/Assets/Scripts/TileMapAlpha.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TileMapAlpha : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        //  Color tmp = GetComponent<Tilemap>().color;
+        // tmp.a = 0f;
+        // GetComponent<Tilemap>().color = tmp;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
